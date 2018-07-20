cc47f4ed45c4e8fae7e40410a8107b8249fe31ed
fendeid <41273072+fendeid@users.noreply.github.com>
2018-07-20 22:11:19 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*- 
 import os
 
-def files():
-	'''Подсчет не пустых файлов в ТЕКУЩЕМ каталоге.'''	
-	#Список не пустых файлов.
-	filelist = [ ]
-	#Обозначаем дерикторию	
-	location = __file__[0 : __file__.find(os.path.basename(__file__))]
-	print(location)	
-	#Перебираем объекты в дериктории		
-	for obj in os.listdir(location):			
-		strings = 0			
-		try:
-			with open(location + obj, 'r') as file:			
-				for string in file:				
-					if file.readline() != '':
-						strings += 1
-				filelist.append([obj, strings])
-		except:
-			pass	
+# Значение 1 считает пустые строки, Значение 2 без них
+turn = 1
+# Путь со скриптом.(начальная директория)
+script_direct = __file__[0 : __file__.find(os.path.basename(__file__))]
+print()
+def bust_script(direct):	
+	# Если директория не пустая
+	if os.listdir(direct):		
+		for obj in os.listdir(direct):
+			# Обновляем количество строк для каждого файла
+			strings = 0	
+			# Если объект является директорией
+			if os.path.isdir(direct + obj) == True:	
+				# Задается новая директория, рекурсивная функция.				
+				new_direct = direct + obj + '\\'
+				bust_script(new_direct)	
+			# Если объект является файлом
+			elif os.path.isfile(direct + obj) == True:
+				#Файл открывается для чтения
+				with open(direct + obj, 'r', encoding = 'utf-8') as file:
+					for line in file:	
+						if turn == 1:	
+							strings += 1
+						elif turn == 2:							
+							for line in file:
+								if file.readline != '\n' and file.readline != '':
+									strings += 1
+			# Вывод 	
+			if os.path.isfile(direct + obj) == True:
+				print('Директория:',direct,'\nИмя:',obj,'\nКоличество строк:', strings)	
+				print('-------------------')
+	# Если директория пустая
+	else:
+		print('Директория:',direct,'\nСтатус: Пуста')
+		print('-------------------')	
 
-	return filelist
-#Присвоили список
-filelist = files()
-
-def statistic(f):
-	'''Вывод статистики'''	
-	for file in f:
-		print('\tName:', file[0], '\tStrings:',file[1])
-statistic(filelist)
-
-
-
-
-
-
-
+bust_script(script_direct)
